018d91c7e68638bf9360d0532d8660272a366924
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2025-08-30 15:39:50 +0300

    Add PrismaModule import to SupportModule for enhanced database integration. This change allows the SupportService to utilize Prisma for data management, improving overall functionality and support capabilities.

--- server/src/modules/support/support.module.ts
+++ server/src/modules/support/support.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SupportService } from './support.service';
 import { SupportController } from './support.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SupportController],
   providers: [SupportService],
   exports: [SupportService],
